2c3cb45bc4328b37c0e5cdecb0a1ee2e88e59b2a
Yura <test@test.com>
2022-04-15 22:52:12 +0300

    start new app

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 "use strict";
-let number = 5;
-const LeftBorderWidth = 1;
 
-number = 10;
-console.log(number);
-// LeftBorderWidth = 10;
-console.log(LeftBorderWidth);
+// let number = 5;
+// const LeftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+// // LeftBorderWidth = 10;
+// console.log(LeftBorderWidth);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt('Один из просмотренных псоледних фильмов ?', ''),
+	b = prompt('На сколько оцените его ?', ''),
+	c = prompt('Один из просмотренных псоледних фильмов ?', ''),
+	d = prompt('На сколько оцените его ?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
